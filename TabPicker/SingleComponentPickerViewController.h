5bb2c0930332453c78d7b905d6bfd7f34dcbfd61
houchenxi <495838087@qq.com>
2015-01-03 18:57:28 +0800

    【增加一个SIngleComponentPickerViewController的相关文件，在TabBarController中创建关联】

---
+++ TabPicker/SingleComponentPickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  SingleComponentPickerViewController.h
+//  TabPicker
+//
+//  Created by houchenxi  on 15-1-3.
+//  Copyright (c) 2015年 haotwo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SingleComponentPickerViewController : UIViewController
+
+@end
---
+++ TabPicker/SingleComponentPickerViewController.m
@@ -0,0 +1,37 @@
+//
+//  SingleComponentPickerViewController.m
+//  TabPicker
+//
+//  Created by houchenxi  on 15-1-3.
+//  Copyright (c) 2015年 haotwo. All rights reserved.
+//
+
+#import "SingleComponentPickerViewController.h"
+
+@interface SingleComponentPickerViewController ()
+
+@end
+
+@implementation SingleComponentPickerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
