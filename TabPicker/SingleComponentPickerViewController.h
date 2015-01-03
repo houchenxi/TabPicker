7d61b02386c7188d26e57b63140ac5ca98fbc217
houchenxi <495838087@qq.com>
2015-01-03 19:28:14 +0800

    【编辑器+代码】完成单组件选择器，非常轻松

--- TabPicker/SingleComponentPickerViewController.h
+++ TabPicker/SingleComponentPickerViewController.h
@@ -9,8 +9,13 @@
 #import <UIKit/UIKit.h>
 
 @interface SingleComponentPickerViewController : UIViewController
+    <UIPickerViewDelegate, UIPickerViewDataSource>
 
 
 @property (weak, nonatomic) IBOutlet UIPickerView *singlePicker;
+
+@property (strong, nonatomic) NSArray* content;
+
+- (IBAction)buttonPressed:(id)sender;
 
 @end
--- TabPicker/SingleComponentPickerViewController.m
+++ TabPicker/SingleComponentPickerViewController.m
@@ -16,7 +16,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    // 选取器的数据数组
+    // NSArray的赋值
+    self.content =
+    @[
+      @"学前班",
+      @"小学",
+      @"初中",
+      @"高中",
+      @"大学",
+      @"研究生"
+      ];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,5 +44,52 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (IBAction)buttonPressed:(id)sender {
+    
+    // 获得0这个下标的组件中选取的行的下标号
+    NSInteger rowSelectedIndex = [self.singlePicker selectedRowInComponent:0];
+    
+    // 获得选取的文本，后面用它显示
+    NSString* textSelected = self.content[rowSelectedIndex];
+    
+    // 用上面选中项的文本构造一个标题
+    NSString* title = [
+        [NSString alloc]
+            initWithFormat:@"你选择了[%@]", textSelected
+         
+    ];
+    
+    
+    // 弹出模态的警告对话框，显示选中文本的相关信息
+    [
+     [[UIAlertView alloc]
+          initWithTitle:title
+              message:@"欢迎选择"
+              delegate:nil
+              cancelButtonTitle:@"关闭"
+              otherButtonTitles:nil
+      ]
+     show];
+}
+
+#pragma mark - 组件数
+- (NSInteger) numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+#pragma mark - 组件中的行数
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return [self.content count];
+}
+
+#pragma mark - 选择器中文字选项的适配器 这是Delegate协议的接口
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    return self.content[row];
+}
+
 
 @end
